3e9a2fde6c4c18448c10cebabf98e144e40c1118
jithendra14377 <trjr0905@gmail.com>
2026-09-06 19:31:51 +0530

    comm

--- cmail.py
+++ cmail.py
@@ -1,14 +1,13 @@
 import smtplib
 from email.message import EmailMessage
-def send_mail(to,subject,body):
-    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('anusha@codegnan.com','btyv ewgb kdev ntvr')
+def send_email(to,subject,body):
+    server =smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('trjr0905@gmail.com','vzfs nilp jenx mozk')
     msg=EmailMessage()
-    msg['FROM']='anusha@codegnan.com'
+    msg['FROM']='trjr0905@gmail.com'
     msg['TO']=to
     msg['SUBJECT']=subject
     msg.set_content(body)
     server.send_message(msg)
-    print('Mail sent')
+    print('Mail Sent')
     server.close()
-
